0f61d5e60182819a6a2e214ba6d3162965f720a7
Alper Kaya <73350743+alperkaya0@users.noreply.github.com>
2022-02-09 02:06:28 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+app.get("/", function(req,res) {
+    res.send("<h1>Mr.Sexo</h1>");    
+})
+let port = 3000;
+app.listen(port, function() {
+    console.log(`Server started on port ${port}... please use the following link to access it http://localhost:${port} or http://127.0.0.1:${port}`);
+});
